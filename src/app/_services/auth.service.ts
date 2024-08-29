f4a186bcb971d34d71fa5ce6796a1d0e796cda6b
EtienneRakotoarison <97359872+EtienneRakotoarison@users.noreply.github.com>
2024-08-30 00:05:30 +0300

    Commit

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const AUTH_API = 'http://localhost:8080/api/auth/';
+const AUTH_API = 'https://127.0.0.1:8000/api/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -16,7 +16,7 @@ export class AuthService {
 
   login(username: string, password: string): Observable<any> {
     return this.http.post(
-      AUTH_API + 'signin',
+      AUTH_API + 'login_check',
       {
         username,
         password,
